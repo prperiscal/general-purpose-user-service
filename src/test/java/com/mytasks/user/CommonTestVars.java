ea2d14d2e135ccc7a9a3d0ce01dbd468e198c9a0
prperiscal <prperiscal@gmail.com>
2018-02-25 22:57:14 +0100

    test(project): add test profile

---
+++ src/test/java/com/mytasks/user/CommonTestVars.java
@@ -0,0 +1,12 @@
+package com.mytasks.user;
+
+/**
+ * <p>Common vars used across tests files.
+ *
+ * @author <a href="mailto:prperiscal@gmail.com">Pablo Rey Periscal</a>
+ * @since 1.2.0
+ */
+public final class CommonTestVars {
+
+    public static final String PROFILE = "test";
+}
